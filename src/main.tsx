e859a0245a26fe6c910242de51e8e7d51f4dcb2b
Yann Darwish <yann.darwish@gmail.com>
2023-03-19 18:24:23 +0100

    redux logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
+import cartReducer from "./state"
 import "./index.css"
+
+const store = configureStore({
+	reducer: { cart: cartReducer },
+})
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 )
---
+++ src/state/index.ts
@@ -0,0 +1,87 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+
+interface Item {
+	id: number
+	title: string
+	description: string
+	price: number
+	discountPercentage: number
+	rating: number
+	stock: number
+	brand: string
+	category: string
+	thumbnail: string
+	images: string[]
+}
+
+interface CartItem {
+	id: number
+	title: string
+	description: string
+	price: number
+	discountPercentage: number
+	rating: number
+	stock: number
+	brand: string
+	category: string
+	thumbnail: string
+	images: string[]
+	count: number
+}
+
+interface CartState {
+	isCartOpen: boolean
+	cart: CartItem[]
+	items: Item[]
+}
+
+const initialState: CartState = {
+	isCartOpen: false,
+	cart: [],
+	items: [],
+}
+
+export const cartSlice = createSlice({
+	name: "cart",
+	initialState,
+	reducers: {
+		setItems: (state, action: PayloadAction<Item[]>) => {
+			state.items = action.payload
+		},
+		addToCart: (state, action: PayloadAction<{ item: CartItem }>) => {
+			state.cart = [...state.cart, action.payload.item]
+		},
+		removeFromCart: (state, action: PayloadAction<{ id: number }>) => {
+			state.cart = state.cart.filter((item) => item.id !== action.payload.id)
+		},
+		increaseCount: (state, action: PayloadAction<CartItem>) => {
+			state.cart = state.cart.map((item) => {
+				if (item.id === action.payload.id) {
+					item.count++
+				}
+				return item
+			})
+		},
+		decreaseCount: (state, action: PayloadAction<CartItem>) => {
+			state.cart = state.cart.map((item) => {
+				if (item.id === action.payload.id && item.count > 1) {
+					item.count--
+				}
+				return item
+			})
+		},
+		setIsCartOpen: (state) => {
+			state.isCartOpen = !state.isCartOpen
+		},
+	},
+})
+
+export const {
+	setItems,
+	addToCart,
+	removeFromCart,
+	increaseCount,
+	decreaseCount,
+	setIsCartOpen,
+} = cartSlice.actions
+export default cartSlice.reducer
